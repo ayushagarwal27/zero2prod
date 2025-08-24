796ebd5dceb87ec331028c061cdb84cf467eeb1d
ayush <ayushagarwal.dev@gmail.com>
2025-08-24 15:05:48 +0530

    add health-check test and basic setup

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use std::net::TcpListener;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    run(listener)?.await
 }
