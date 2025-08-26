71883f65e33d8273c58ed7802344a8d1fb21d709
ayush <ayushagarwal.dev@gmail.com>
2025-08-26 22:37:48 +0530

    add tests ans setup for subscriptions post endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,4 @@
-use std::net::TcpListener;
+use std::{fmt::format, net::TcpListener};
 
 #[tokio::test]
 async fn health_check_works() {
@@ -16,6 +16,56 @@ async fn health_check_works() {
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_return_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let body = "name=le%20guin&email=le_guin%40email.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
+#[tokio::test]
+async fn subscribe_return_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=john%20doe", "missing the email"),
+        ("email=johndoe%40email.com", "missing the name"),
+        ("", "missing both email and name"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "This API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        )
+    }
 }
 
 fn spawn_app() -> String {
