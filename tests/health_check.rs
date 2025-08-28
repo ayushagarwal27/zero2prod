0594fd95f126dce29ac4f76c0d97ebaa568a90b5
ayush <ayushagarwal.dev@gmail.com>
2025-08-28 19:16:17 +0530

    add configuration code and refactor code for connecting to db

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,7 @@
-use std::{fmt::format, net::TcpListener};
+use std::net::TcpListener;
+
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration::get_configuration;
 
 #[tokio::test]
 async fn health_check_works() {
@@ -22,6 +25,12 @@ async fn health_check_works() {
 async fn subscribe_return_a_200_for_valid_form_data() {
     // Arrange
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let client = reqwest::Client::new();
 
     // Act
@@ -36,6 +45,14 @@ async fn subscribe_return_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Fetch to fetch failed subscriptions");
+
+    assert_eq!(saved.email, "le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 #[tokio::test]
 async fn subscribe_return_a_400_when_data_is_missing() {
@@ -71,7 +88,7 @@ async fn subscribe_return_a_400_when_data_is_missing() {
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind the port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
